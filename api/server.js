95b5c10cf840a1e13e9da637f90ef44abf8a357c
fritzgt <tuanis@live.com>
2019-08-30 07:12:54 +0300

    passed the get request test

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const server = express();
 
+const db = require('../test-model');
+
 server.use(express.json());
 
+//for testing purposes
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
 });
+
+module.exports = server;
